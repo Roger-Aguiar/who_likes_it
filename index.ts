d25dddbbafed104fb057541e2d06a6e65d3483c6
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-10-28 09:22:39 -0300

    It drops some unused code.

--- index.ts
+++ index.ts
@@ -2,15 +2,3 @@ import { Likes } from "./src/likes";
 
 let numberOfLikes = new Likes(["Alex", "Jacob", "Mark", "Max", "Roger", "André", "Robson", "Lázara", "Marcus", "Luiz"]);
 console.log(numberOfLikes.getLikes());
-
-/*import readline = require('readline');
-
-let userInput = readline.createInterface({input: process.stdin, output: process.stdout});
-
-userInput.question("Please, enter an integer number: ", 
-function(answer)
-{
-    let integerNumber = new SumOfDigits(answer);
-    let digitalRootSum = integerNumber.digitalRoot();
-    console.log(`The digital root sum is: ${digitalRootSum}`);
-});*/
