a652812cc9061cbd9b0978e8366f118df5945a9f
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-10-28 08:49:11 -0300

    It creates the initial files and sets up the project.

---
+++ index.ts
@@ -0,0 +1,11 @@
+/*import readline = require('readline');
+
+let userInput = readline.createInterface({input: process.stdin, output: process.stdout});
+
+userInput.question("Please, enter an integer number: ", 
+function(answer)
+{
+    let integerNumber = new SumOfDigits(answer);
+    let digitalRootSum = integerNumber.digitalRoot();
+    console.log(`The digital root sum is: ${digitalRootSum}`);
+});*/
